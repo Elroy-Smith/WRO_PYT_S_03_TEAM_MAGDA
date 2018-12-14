d6cf10e7d151a2388ef8b15aa014b6905e24bc6c
Milan Nowak <milan.nowak90@gmail.com>
2018-12-14 14:53:31 +0100

    cos

--- IMAGEY/imagey/models.py
+++ IMAGEY/imagey/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Image(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(null=True)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     creation_date = models.DateField(auto_now_add=True)
     image = models.ImageField(upload_to="images")
